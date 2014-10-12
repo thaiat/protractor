b1fba597b83dd589c8aeaa1121740f33a6fadea4
thaiat <thaiat@ipelia.fr>
2014-10-12 13:11:31 +0000

    WIP

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+var gulp = require('gulp');
+require('gulp-help')(gulp);
+require('require-dir')('./gulp/tasks');
+
+// add your top gulp tasks here
+gulp.task('default', function() {
+
+});
---
+++ protractor.conf.js
@@ -0,0 +1,42 @@
+// conf.js
+exports.config = {
+    //seleniumAddress: 'http://0.0.0.0:4444/wd/hub',
+    seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.43.1.jar',
+    specs: ['spec.js'],
+    capabilities: {
+        browserName: 'phantomjs',
+        version: '',
+        platform: 'ANY',
+        'phantom.binary.path': 'node_modules/phantomjs/lib/phantom/bin/phantomj'
+    },
+    jasmineNodeOpts: {
+        showColors: true,
+        silent: true,
+        defaultTimeoutInterval: 30000
+    },
+    onPrepare: function() {
+        browser.driver.manage().window().setSize(1600, 800);
+        var folderName = ''; // (new Date()).toString().split(' ').splice(1, 4).join(' ');
+        var mkdirp = require('mkdirp');
+        var newFolder = "./reports/" + folderName;
+        require('jasmine-reporters');
+        require('jasmine-spec-reporter');
+        var HtmlReporter = require('protractor-html-screenshot-reporter');
+        mkdirp(newFolder, function(err) {
+            if(err) {
+                console.error(err);
+            } else {
+                //jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('reports', true, true));
+                //jasmine.getEnv().addReporter(new jasmine.ConsoleReporter());
+                jasmine.getEnv().addReporter(new jasmine.SpecReporter({
+                    displayStacktrace: true
+                }));
+                jasmine.getEnv().addReporter(new HtmlReporter({
+                    baseDirectory: 'reports/screenshots',
+                    docName: 'index.html',
+                    takeScreenShotsOnlyForFailedSpecs: false
+                }));
+            }
+        });
+    },
+};
